9fef23769a9e5f218a65ff10e7e389fca77db8e7
perey <perey@DESKTOP-083PCVM>
2021-10-05 19:41:46 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppAhorcado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppAhorcado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
