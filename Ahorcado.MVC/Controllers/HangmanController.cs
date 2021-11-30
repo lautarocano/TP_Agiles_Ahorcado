3bca884af01c6ef1297f38f400ca3095f8e9d425
Martin Pereyra <mapereyra@frro.utn.edu.ar>
2021-11-30 18:01:37 -0300

    proyecto ui agregado

---
+++ Ahorcado.MVC/Controllers/HangmanController.cs
@@ -0,0 +1,48 @@
+﻿using Ahorcado.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ahorcado.MVC.Controllers
+{
+    public class HangmanController : Controller
+    {
+        public static Juego Juego { get; set; }
+
+        // GET: Hangman
+        public ActionResult Index()
+        {
+            return View(new Hangman());
+        }
+
+        [HttpPost]
+        public JsonResult InsertWordToGuess(Hangman model)
+        {
+            Juego = new Juego(model.WordToGuess);
+            model.ChancesLeft = Juego.intentosRestantes;
+            return Json(model);
+        }
+
+        [HttpPost]
+        public JsonResult TryLetter(Hangman model)
+        {
+            Juego.validarLetra(Convert.ToChar(model.LetterTyped));
+            model.Win = Juego.checkearEstadoActual();
+            model.ChancesLeft = Juego.intentosRestantes;
+            model.WrongLetters = string.Empty;
+            foreach (var wLetter in Juego.letrasErradas)
+            {
+                model.WrongLetters += wLetter +  ",";
+            }
+            model.GuessingWord = string.Empty;
+            foreach (var rLetter in Juego.estadoAux)
+            {
+                model.GuessingWord += rLetter + " ";
+            }
+            model.LetterTyped = string.Empty;
+            return Json(model);
+        }
+    }
+}
---
+++ Ahorcado.UIAutomation/HangmanSpecflow.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+using System.Threading;
+
+namespace Ahorcado.UIAutomation
+{
+    [Binding]
+    public class HangmanSpecflow
+    {
+        IWebDriver driver;
+        String baseURL;
+        Juego j;
+
+        [BeforeScenario]
+        public void TestInitialize()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory + @"\Drivers";
+            //driver = new InternetExplorerDriver(path);
+            driver  = new ChromeDriver(path);
+            baseURL = "http://localhost:39278/";
+        }
+
+        [Given(@"I have entered Ahorcado as the wordToGuess")]
+        public void GivenIHaveEnteredAhorcadoAsTheWordToGuess()
+        {
+            j = new Juego("Ahorcado");
+            driver.Navigate().GoToUrl(baseURL);
+
+            Thread.Sleep(5000);
+            
+            var txtPalabra = driver.FindElement(By.Id("WordToGuess"));
+            txtPalabra.SendKeys("Ahorcado");
+
+            Thread.Sleep(1000);
+
+            var btnInsertWord = driver.FindElement(By.Id("btnInsertWord"));
+            btnInsertWord.SendKeys(Keys.Enter);
+
+            Thread.Sleep(1000);
+        }
+               
+        [When(@"I enter X as the typedLetter five times")]
+        public void WhenIEnterXAsTheTypedLetterFiveTimes()
+        {
+            var letterTyped = driver.FindElement(By.Id("LetterTyped"));
+            var btnInsertLetter = driver.FindElement(By.Id("btnInsertLetter"));
+            for (int i = 0; i < 7; i++)
+            {
+                letterTyped.SendKeys("X");
+                Thread.Sleep(1000);
+                btnInsertLetter.SendKeys(Keys.Enter);
+            }
+        }
+
+        [Then(@"I should be told that I lost")]
+        public void ThenIShouldBeToldThatILost()
+        {
+            var chancesLeft = driver.FindElement(By.Id("ChancesLeft"));
+            var loss = Convert.ToInt32(chancesLeft.GetAttribute("value")) == 0;
+            Thread.Sleep(1000);
+            Assert.IsTrue(loss);
+            Thread.Sleep(1000);
+        }
+
+        [AfterScenario]
+        public void TestCleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
